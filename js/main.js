0e1cc70a21f7d1b076014083b5cde707ffcb6454
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-04-08 15:50:49 +0200

    14-rel

--- js/main.js
+++ js/main.js
@@ -1,72 +1,75 @@
 $(document).ready(function () {
 
-
-$.ajax({
-	url: 'http://157.230.17.132:4009/sales',
-	method:'GET',
-	success: function(data) {
-		var revenuesData = data;
-		console.log(revenuesData);
-		var montlyRevenues = {};
-		console.log(montlyRevenues);
-		var salesManRevenues = {};
-		var totalRevenue = 0;
-
-		for (var i = 0; i < revenuesData.length; i++) {
-			var revenueData = revenuesData[i];
-			console.log(revenueData);
-			var date = revenueData.date;
-			var salesMan = revenueData.salesman;
-			var isoDate = moment(date,"DD/MM/YYYY");
-			var monthName = isoDate.month();
-			var labelRev = [];
-			var dataRev = [];
-			console.log(totalRevenue);
-			var salesManlabel = [];
-			var salesMandata = [];
+	buildDashboard();
 
 
-			if (montlyRevenues[monthName] === undefined) {
-				montlyRevenues[monthName] = 0;
+
+
+
+
+
+
+
+
+function buildDashboard() {
+	$.ajax({
+		url: 'http://157.230.17.132:4009/sales',
+		method:'GET',
+		success: function(data) {
+			var revenuesData = data;
+			console.log(revenuesData);
+			var montlyRevenues = {};
+			console.log(montlyRevenues);
+			var salesManRevenues = {};
+			var totalRevenue = 0;
+
+			for (var i = 0; i < revenuesData.length; i++) {
+				var revenueData = revenuesData[i];
+				console.log(revenueData);
+				var date = revenueData.date;
+				var salesMan = revenueData.salesman;
+				var isoDate = moment(date,"DD/MM/YYYY");
+				var monthName = isoDate.month();
+				var labelRev = [];
+				var dataRev = [];
+				console.log(totalRevenue);
+				var salesManlabel = [];
+				var salesMandata = [];
+
+
+				if (montlyRevenues[monthName] === undefined) {
+					montlyRevenues[monthName] = 0;
+				};
+
+				if (salesManRevenues[salesMan] === undefined) {
+					salesManRevenues[salesMan] = 0;
+				};
+				montlyRevenues[monthName] += revenueData.amount;
+				salesManRevenues[salesMan] += revenueData.amount;
+				totalRevenue += revenueData.amount;
+
+
 			};
 
-			if (salesManRevenues[salesMan] === undefined) {
-				salesManRevenues[salesMan] = 0;
+			for (var key in montlyRevenues) {
+				labelRev.push(moment().month(key).format('MMMM'));
+				dataRev.push(montlyRevenues[key]);
 			};
-			montlyRevenues[monthName] += revenueData.amount;
-			salesManRevenues[salesMan] += revenueData.amount;
-			totalRevenue += revenueData.amount;
 
+			for (var key in salesManRevenues) {
+				salesManlabel.push(key);
+				salesMandata.push((salesManRevenues[key]/totalRevenue));
+			};
 
-		};
+			console.log(salesManlabel);
+			console.log(salesMandata);
 
-		for (var key in montlyRevenues) {
-			labelRev.push(moment().month(key).format('MMMM'));
-			dataRev.push(montlyRevenues[key]);
-		};
+			chart(lineChartData('line',dataRev,labelRev),'montly-revenue');
+			chart(pieChartData('pie',salesMandata,salesManlabel),'annual-revenuexsalesman');
 
-		for (var key in salesManRevenues) {
-			salesManlabel.push(key);
-			salesMandata.push((salesManRevenues[key]/totalRevenue));
-		};
-
-		console.log(salesManlabel);
-		console.log(salesMandata);
-
-		chart(lineChartData('line',dataRev,labelRev),'montly-revenue');
-		chart(pieChartData('pie',salesMandata,salesManlabel),'annual-revenuexsalesman');
-
+			}
+		});
 	}
-});
-
-
-
-
-
-
-
-
-
 
 
 	function chart(data,selector) {
